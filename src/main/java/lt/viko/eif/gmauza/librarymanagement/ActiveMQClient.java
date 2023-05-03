280565990765dffa5e9e36ed8de8376b35764b4b
GiedriusMauza <giedrius.mauza@gmail.com>
2023-05-03 20:16:21 +0300

    ActiveMQClient and ActiveMQServer added to the project

---
+++ src/main/java/lt/viko/eif/gmauza/librarymanagement/ActiveMQClient.java
@@ -0,0 +1,71 @@
+package lt.viko.eif.gmauza.librarymanagement;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * Hello world!
+ */
+public class ActiveMQClient {
+
+    public static void main(String[] args) throws Exception {
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldConsumer(), false);
+        Thread.sleep(1000);
+
+    }
+
+    public static void thread(Runnable runnable, boolean daemon) {
+        Thread brokerThread = new Thread(runnable);
+        brokerThread.setDaemon(daemon);
+        brokerThread.start();
+    }
+
+    public static class HelloWorldConsumer implements Runnable, ExceptionListener {
+        public void run() {
+            try {
+
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                connection.setExceptionListener(this);
+
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("LibraryQueue");
+
+                // Create a MessageConsumer from the Session to the Topic or Queue
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                // Wait for a message
+                Message message = consumer.receive(1000);
+
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    String text = textMessage.getText();
+                    System.out.println("Received: " + text);
+                } else {
+                    System.out.println("Received: " + message);
+                }
+
+                consumer.close();
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        public synchronized void onException(JMSException ex) {
+            System.out.println("JMS Exception occured.  Shutting down client.");
+        }
+    }
+}
