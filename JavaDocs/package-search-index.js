c72f5e2e72c9807d4ab627752c6cbf99a4cc86d9
GiedriusMauza <giedrius.mauza@gmail.com>
2023-05-08 21:06:47 +0300

    JavaDocs added

---
+++ JavaDocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lt.viko.eif.gmauza.librarymanagement"},{"l":"lt.viko.eif.gmauza.librarymanagement.model"},{"l":"lt.viko.eif.gmauza.librarymanagement.utils"}];updateSearchResults();
